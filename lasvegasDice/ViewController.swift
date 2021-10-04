277ced4599d65d00d2a10d0aa0e3bf85c6c8e2a1
TruongNH <huynhtruong0316@gmail.com>
2021-10-04 15:32:45 +0700

    Initial Commit

---
+++ lasvegasDice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lasvegasDice
+//
+//  Created by TruongNH on 10/4/21.
+//  Copyright © 2021 TruongNH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
